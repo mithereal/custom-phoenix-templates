15e05069270c133af0e32a2da3e57b81c5682812
Jason Clark <mithereal@gmail.com>
2018-12-11 17:31:47 -0700

    Update partials_view.ex

--- apps/xxx_web/priv/templates/default.gen.html/partials_view.ex
+++ apps/xxx_web/priv/templates/default.gen.html/partials_view.ex
@@ -1,3 +1,3 @@
 defmodule <%= module %>PartialsView do
-use BrokerWeb, {:view, %{root: "lib/<%= base %>_web/templates/<%= singular %>/partials", path: ""}}
+use <%= base %>Web, {:view, %{root: "lib/<%= base %>_web/templates/<%= singular %>/partials", path: ""}}
 end
